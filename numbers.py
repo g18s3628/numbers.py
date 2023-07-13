175d97d323c4c3f8feb746e5a66c9dabccdc4ba7
g18s3628 <36521304+g18s3628@users.noreply.github.com>
2023-07-13 23:52:21 +0200

    numbers.py

---
+++ numbers.py
@@ -0,0 +1,20 @@
+#Request uer to enter three different integers
+int1 = int(input("Enter the first integer:"))
+int2 = int(input("Enter the second integer:"))
+int3 = int(input("Enter the third integer:"))
+
+#Summation of the three intergers 
+int_sum = int1 + int2 + int3 #Addition of all three integers 
+print("Summation of integers = ", int_sum)
+
+#Difference between the first and third integer
+int_diff = int1 - int2 #Subtracting integer 1 minus integer 2 
+print("Difference of integer 1 and integer 2 =", int_diff)
+
+#Multiplication of the third and first integer
+int_mult = int3 * int1 #Multipying integer 3 and integer 1
+print("Muliplication of integer 3 and intger 3: ", int_mult)
+
+#Summation of all three integers divided by the third integer
+int_div = (int1 + int2 + int3)/int3 #SSummation of all three integer divided by integer three  
+print("Dividing all three integers divided by the third number:", int_div)
